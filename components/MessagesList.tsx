719eca3cee2644cb047038a293bd3ebd646b44fe
Julien Blatecky <julien.blatecky@hey.com>
2021-11-23 15:42:06 +0100

    Add network logic

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -1,32 +1,47 @@
+import useSWR from 'swr'
 import Message from './Message'
 
+interface MessageApi {
+    author: string
+    content: string
+    timestamp: number
+}
+
+const fetcher = async (url: string) => {
+    let pageIndex = 0
+    let messages: MessageApi[] = []
+    let finished = false
+
+    while (!finished) {
+        const urlWithPage = `${url}?page=${pageIndex}`
+        const response = await fetch(urlWithPage).then((response) => response.json())
+
+        // Merge pages
+        messages = messages.concat(response)
+
+        // Increase page and finish loop if no message
+        pageIndex += 1
+        finished = response.length === 0
+    }
+
+    // Return messages in reverse order
+    return messages.reverse()
+}
+
 const MessagesList = () => {
-    const messages = [
-        {
-            body: "Message",
-            author: "Julien",
-            date: new Date()
-        },
-        {
-            body: "Message test",
-            author: "Julien 2",
-            date: new Date()
-        },
-        {
-            body: "Message test 2",
-            author: "Julien 3",
-            date: new Date()
-        },
-        {
-            body: "Message test 3",
-            author: "Julien 4",
-            date: new Date()
-        }
-    ]
+    const { data, error } = useSWR<MessageApi[]>('https://ensmn.herokuapp.com/messages', fetcher)
+
+    if (error) {
+        return <div>failed to load</div>
+    }
+
+    if (!data) {
+        return <div>loading...</div>
+    }
 
     return (
         <div className="container">
-            {messages.map(({ body, author, date }, i) => <Message key={i} body={body} author={author} date={date} />)}
+            {data.map(({ content, author, timestamp }, i) => <Message key={i} body={content} author={author} date={new Date(timestamp)} />)}
         </div>
     )
 }
--- components/NewMessageForm.tsx
+++ components/NewMessageForm.tsx
@@ -1,14 +1,53 @@
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { mutate } from "swr";
+import Message from "./Message";
+
 const NewMessageForm = () => {
+    const [pseudo, setPseudo] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handlePseudoChange = (event: ChangeEvent<HTMLInputElement>) =>
+        setPseudo(event.target.value);
+    const handleMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) =>
+        setMessage(event.target.value);
+
+    const handleSubmit = async (event: SyntheticEvent) => {
+        // Prevent browser to submit
+        event.preventDefault()
+
+        // Validate data
+        if (pseudo.length === 0 || message.length === 0) {
+            return
+        }
+
+        // Send data
+        await fetch("https://ensmn.herokuapp.com/messages", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: pseudo,
+                message: message
+            })
+        })
+
+        // Refresh messages
+        mutate("https://ensmn.herokuapp.com/messages")
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="pseudo" className="form-label">Pseudo</label>
-                <input type="text" className="form-control" id="pseudo" name="pseudo" />
+                <input type="text" className="form-control" id="pseudo" name="pseudo" required onChange={handlePseudoChange} />
             </div>
             <div className="mb-3">
                 <label htmlFor="message" className="form-label">Message</label>
-                <textarea rows={5} className="form-control" id="message"></textarea>
+                <textarea rows={5} className="form-control" id="message" required onChange={handleMessageChange}></textarea>
             </div>
+            <h2>Preview</h2>
+            <Message body={message} author={pseudo} date={new Date()} />
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
